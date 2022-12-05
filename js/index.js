503dc1708006d110e7c03a61ae4c36b1f2410014
yorikhu <shallowink1999@163.com>
2022-12-05 14:35:38 +0800

    feat: 文章链接跳转配置

--- js/index.js
+++ js/index.js
@@ -8,3 +8,8 @@ indexPageList.forEach((item) => {
 });
 
 $('#pagesList').append(tempPageList.join(''));
+
+// 给页面文章绑定点击事件，点击跳转到对应文章地址
+$('#pagesList .page-content').click((e) => {
+  window.open($(e.currentTarget).attr('link'), '_blank');
+});
